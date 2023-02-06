856c0e7db0ed1ca24fdc76eb06e13e9a0c2d665a
leolondono19 <leoneelondono@gmail.com>
2023-02-05 20:36:11 -0400

    solution created

--- src/TicTacToe_Game/TicTacToe_Game/TicTacToe_Game.cpp
+++ src/TicTacToe_Game/TicTacToe_Game/TicTacToe_Game.cpp
@@ -17,19 +17,19 @@ void ask_position() {
 }
 
 void mostrar_matrix() {
-/*
- x | x | x
----|---|---
- x | x | x
----|---|---
- x | x | x
-*/
+    /*
+     x | x | x
+    ---|---|---
+     x | x | x
+    ---|---|---
+     x | x | x
+    */
     for (int i = 0; i < 3; i++)
     {
         if (i != 0) {
             cout << "--|---|--";
         }
-        
+
         cout << endl;
         for (int j = 0; j < 3; j++)
         {
@@ -136,7 +136,7 @@ bool winner() {
 bool is_there_space() {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            if (matrix1[i][j]==' ') {
+            if (matrix1[i][j] == ' ') {
                 return true;
             }
         }
@@ -147,7 +147,7 @@ void pausa() {
     system("pause");
 }
 int main() {
-    int n = 0; string name1;  string name2; char s1=' '; char s2=' ';
+    int n = 0; string name1;  string name2; char s1 = ' '; char s2 = ' ';
     //cout << "Ingrese su ficha: "; cin >> n;
     //matrix[2][2] = ;
     //arrays
@@ -165,19 +165,19 @@ int main() {
         cin >> i;
         switch (i)
         {
-        case 1: 
-            player_names(); 
-            pausa(); 
+        case 1:
+            player_names();
+            pausa();
             break;
         case 2:
-            while (is_there_space() && winner()!=true) {
+            while (is_there_space() && winner() != true) {
                 mostrar_matrix();
                 ask_position();
                 cout << endl;
                 positions_player1();
                 mostrar_matrix1();
                 pausa();
-                if (is_there_space() && winner()!=true) {
+                if (is_there_space() && winner() != true) {
                     mostrar_matrix();
                     ask_position();
                     cout << endl;
@@ -187,7 +187,7 @@ int main() {
                 }
             }
             break;
-        case 3: 
+        case 3:
         case 4: exit(1);
         }
     }
